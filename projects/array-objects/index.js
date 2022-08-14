aae9443ab7afc6658ce0777d18ae6edddeb4f2e2
Alfiya <al.yausheva@gmail.com>
2022-08-14 11:43:59 +0300

    added task 5

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -104,8 +104,15 @@ function upperProps(obj) {
    obj.foo = 2;
    console.log(obj.foo); // 4
  */
-// function createProxy(obj) {
-
-// }
-
-export { forEach, map, reduce, upperProps /*createProxy*/ };
+function createProxy(obj) {
+  return new Proxy(obj, {
+    set(obj, key, value) {
+      obj[key] = value ** 2;
+      return true;
+    },
+  });
+}
+// const obj = createProxy({});
+// obj.foo = 2;
+// console.log(obj.foo); // 4
+export { forEach, map, reduce, upperProps, createProxy };
